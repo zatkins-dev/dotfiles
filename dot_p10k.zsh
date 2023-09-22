4028c26749e09edf7dbfc922968f3b0f42508079
Zach Atkins <zach.atkins@colorado.edu>
2023-09-22 17:27:42 -0600

    redhat fix

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -724,7 +724,7 @@ typeset -g POWERLEVEL9K_DIR_CLASSES=(
   # Context format when running with privileges: user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_TEMPLATE='%n'
   # Context format when in SSH without privileges: user@hostname.
-  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE='%n@%m'
+  typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_TEMPLATE=${USER//\%/%%}'@%m'
   # Default context format (no privileges, no SSH): user@hostname.
   typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n'
 
